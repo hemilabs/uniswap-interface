5f61b52cfe80a00fd44fefe9e91c1501cbb9c78a
Leandro Almeida <leandro@bloq.com>
2024-04-15 10:20:59 -0300

    Hide "Buy" tab

--- apps/web/src/components/swap/SwapHeader.tsx
+++ apps/web/src/components/swap/SwapHeader.tsx
@@ -11,7 +11,6 @@ import { useCallback, useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { RowBetween, RowFixed } from '../Row'
 import SettingsTab from '../Settings'
-import SwapBuyFiatButton from './SwapBuyFiatButton'
 import { SwapTab } from './constants'
 import { SwapHeaderTabButton } from './styled'
 
@@ -105,7 +104,6 @@ export default function SwapHeader({ compact, syncTabToUrl }: { compact: boolean
             <Trans>Send</Trans>
           </SwapHeaderTabButton>
         )}
-        <SwapBuyFiatButton />
       </HeaderButtonContainer>
       {currentTab === SwapTab.Swap && (
         <RowFixed>
