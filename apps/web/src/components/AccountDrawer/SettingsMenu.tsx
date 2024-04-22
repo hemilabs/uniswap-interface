d728f471f2cc12cd44be08930b6a81bec58bd9a3
Leandro Almeida <leandro@bloq.com>
2024-04-22 12:01:58 -0300

    Disable dark mode settings

--- apps/web/src/components/AccountDrawer/SettingsMenu.tsx
+++ apps/web/src/components/AccountDrawer/SettingsMenu.tsx
@@ -90,7 +90,7 @@ export default function SettingsMenu({
             <Trans>Preferences</Trans>
           </SectionTitle>
           <ToggleWrapper currencyConversionEnabled={currencyConversionEnabled}>
-            <ThemeToggle />
+            {/*<ThemeToggle /> TODO: Enable when dark mode is completed ref: https://github.com/hemilabs/pure.finance/issues/31 */}
             <SmallBalanceToggle />
             <SpamToggle />
             <AnalyticsToggle />
